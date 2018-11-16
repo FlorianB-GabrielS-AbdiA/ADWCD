384253f441d69090f577f2b432c8075650f1f6f0
FlorianB-GabrielS-AbdiA <43742878+FlorianB-GabrielS-AbdiA@users.noreply.github.com>
2018-11-16 12:34:13 +0100

    Add files via upload

---
+++ src/www/main.js
@@ -0,0 +1,31 @@
+<!doctype html>
+body {
+	background :#735144
+	backgroundColor
+	backgroundImage
+}
+ 
+#main{
+	width :100 px
+	height :200 px
+	background : #3399FF
+	margin
+	border : solid 2px #3399FF
+}
+
+hl{
+	textAlign 
+	fontcolor
+	fontSize
+	color
+}
+
+p{
+	fontFamily
+	fontSize
+	fontWeight
+}
+
+hr{
+	height : 2px
+	
